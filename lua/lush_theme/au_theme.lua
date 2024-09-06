ef9e8af1b91d619187c474f8151d3da05b7fe09d
Aiden Townsend <aidenticklesgood@gmail.com>
2024-09-05 19:42:57 -0500

    Adjust Visual color

--- lua/lush_theme/au_theme.lua
+++ lua/lush_theme/au_theme.lua
@@ -1,31 +1,31 @@
-local lush = require('lush')
-local hsl = lush.hsl
+local lush      = require('lush')
+local hsl       = lush.hsl
 local sea_foam  = hsl(208, 100, 80) -- Vim has a mapping, <n>C-a and <n>C-x to
 local sea_crest = hsl(208, 90, 30)  -- increment or decrement integers, or
-local orange = sea_crest.ro(180).li(10)
+local orange    = sea_crest.ro(180).li(10)
 local sea_deep  = hsl(208, 90, 10)  -- you can just type them normally.
 local sea_gull  = hsl("#c6c6c6")
 
 ---@diagnostic disable: undefined-global
-local theme = lush(function(injected_functions)
+local theme     = lush(function(injected_functions)
   local sym = injected_functions.sym
   return {
-     Normal { bg = sea_deep.li(3), fg = sea_foam.lighten(50) }, -- Goodbye gray, hello blue!
-     Cursor         {bg = Normal.fg.da(20), fg = Normal.bg}, -- Character under the cursor
-     CursorLine { bg = Normal.bg.lighten(5) }, -- lighten() can also be called via li()
-     LineNr { fg = orange, bg = sea_deep, gui = "italic" },
-     CursorLineNr { LineNr, fg = orange.li(20), gui = "bold" },
-     Visual { fg = Normal.bg, bg = Normal.fg.da(10) }, -- Try pressing v and selecting some text
-     Comment { fg = Normal.bg.li(25).ro(-20) },
-     Substitute     {bg = orange.ro(-10).li(30), fg = Normal.fg}, -- |:substitute| replacement text highlighting
-     VertSplit      {fg = sea_crest, bg = orange.ro(-10)}, -- Column separating vertically split windows
-     Search         {bg= orange.ro(-10).li(15), fg = Normal.fg}, -- Last search pattern highlighting (see 'hlsearch'). Also used for similar items that need to stand out.
+    Normal { bg = sea_deep.li(3), fg = sea_foam.lighten(50) },  -- Goodbye gray, hello blue!
+    Cursor { bg = Normal.fg.da(20), fg = Normal.bg },           -- Character under the cursor
+    CursorLine { bg = Normal.bg.lighten(5) },                   -- lighten() can also be called via li()
+    LineNr { fg = orange, bg = sea_deep, gui = "italic" },
+    CursorLineNr { LineNr, fg = orange.li(20), gui = "bold" },
+    Visual { fg = Normal.bg, bg = Normal.fg.da(40).de(35) },      -- Try pressing v and selecting some text
+    Comment { fg = Normal.bg.li(25).ro(-20) },
+    Substitute { bg = orange.ro(-10).li(30), fg = Normal.fg },    -- |:substitute| replacement text highlighting
+    VertSplit { fg = sea_crest, bg = orange.ro(-10) },            -- Column separating vertically split windows
+    Search { bg = orange.ro(-10).li(15), fg = Normal.fg },        -- Last search pattern highlighting (see 'hlsearch'). Also used for similar items that need to stand out.
     -- ColorColumn    { }, -- Columns set with 'colorcolumn'
     -- Conceal        { }, -- Placeholder characters substituted for concealed text (see 'conceallevel')
-     CurSearch      {bg= orange.ro(-10).li(15), fg = Normal.fg}, -- Highlighting a search pattern under the cursor (see 'hlsearch')
+    CurSearch { bg = orange.ro(-10).li(15), fg = Normal.fg },    -- Highlighting a search pattern under the cursor (see 'hlsearch')
     -- lCursor        { }, -- Character under the cursor when |language-mapping| is used (see 'guicursor')
     -- CursorIM       { }, -- Like Cursor, but used when in IME mode |CursorIM|
-     --CursorColumn   { }, -- Screen-column at the cursor, when 'cursorcolumn' is set.
+    --CursorColumn   { }, -- Screen-column at the cursor, when 'cursorcolumn' is set.
     -- Directory      { }, -- Directory names (and other special names in listings)
     -- DiffAdd        { }, -- Diff mode: Added line |diff.txt|
     -- DiffChange     { }, -- Diff mode: Changed line |diff.txt|
@@ -43,15 +43,15 @@ local theme = lush(function(injected_functions)
     -- LineNrBelow    { }, -- Line number for when the 'relativenumber' option is set, below the cursor line
     -- CursorLineFold { }, -- Like FoldColumn when 'cursorline' is set for the cursor line
     -- CursorLineSign { }, -- Like SignColumn when 'cursorline' is set for the cursor line
-     MatchParen     {bg= sea_deep.de(70).li(30), fg=orange.li(30)}, -- Character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
+    MatchParen { bg = sea_deep.de(70).li(30), fg = orange.li(30) }, -- Character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
     -- ModeMsg        { }, -- 'showmode' message (e.g., "-- INSERT -- ")
-     --MsgArea        { }, -- Area for messages and cmdline
+    --MsgArea        { }, -- Area for messages and cmdline
     -- MsgSeparator   { }, -- Separator for scrolled messages, `msgsep` flag of 'display'
     -- MoreMsg        { }, -- |more-prompt|
     -- NonText        { }, -- '@' at the end of the window, characters from 'showbreak' and other characters that do not really exist in the text (e.g., ">" displayed when a double-wide character doesn't fit at the end of the line). See also |hl-EndOfBuffer|.
-     --NormalFloat    { }, -- Normal text in floating windows.
-     --FloatBorder    { }, -- Border of floating windows.
-     --FloatTitle     { }, -- Title of floating windows.
+    --NormalFloat    { }, -- Normal text in floating windows.
+    --FloatBorder    { }, -- Border of floating windows.
+    --FloatTitle     { }, -- Title of floating windows.
     -- NormalNC       { }, -- normal text in non-current windows
     -- Pmenu          { }, -- Popup menu: Normal item.
     -- PmenuSel       { }, -- Popup menu: Selected item.
@@ -68,31 +68,31 @@ local theme = lush(function(injected_functions)
     -- SpellCap       { }, -- Word that should start with a capital. |spell| Combined with the highlighting used otherwise.
     -- SpellLocal     { }, -- Word that is recognized by the spellchecker as one that is used in another region. |spell| Combined with the highlighting used otherwise.
     -- SpellRare      { }, -- Word that is recognized by the spellchecker as one that is hardly ever used. |spell| Combined with the highlighting used otherwise.
-     StatusLine     {bg = orange.li(15)}, -- Status line of current window
+    StatusLine { bg = orange.li(15) },    -- Status line of current window
     -- StatusLineNC   { }, -- Status lines of not-current windows. Note: If this is equal to "StatusLine" Vim will use "^^^" in the status line of the current window.
-     --TabLine        { }, -- Tab pages line, not active tab page label
+    --TabLine        { }, -- Tab pages line, not active tab page label
     -- TabLineFill    { }, -- Tab pages line, where there are no labels
     -- TabLineSel     { }, -- Tab pages line, active tab page label
-     --Title          { }, -- Titles for output from ":set all", ":autocmd" etc.
+    --Title          { }, -- Titles for output from ":set all", ":autocmd" etc.
     -- VisualNOS      { }, -- Visual mode selection when vim is "Not Owning the Selection".
     -- WarningMsg     { }, -- Warning messages
-     --Whitespace     { }, -- "nbsp", "space", "tab" and "trail" in 'listchars'
+    --Whitespace     { }, -- "nbsp", "space", "tab" and "trail" in 'listchars'
     -- Winseparator   { }, -- Separator between window splits. Inherts from |hl-VertSplit| by default, which it will replace eventually.
     -- WildMenu       { }, -- Current match in 'wildmenu' completion
-     --WinBar         { }, -- Window bar of current window
-     --WinBarNC       { }, -- Window bar of not-current windows
+    --WinBar         { }, -- Window bar of current window
+    --WinBarNC       { }, -- Window bar of not-current windows
 
-     Constant       {fg = hsl("#B3FFF7")}, -- (*) Any constant
-     String         {fg = hsl("#5CFF4A")}, --   A string constant: "this is a string"
-     Character      {fg = String.fg.desaturate(40).ro(15)}, --   A character constant: 'c', '\n'
+    Constant { fg = hsl("#B3FFF7") },                       -- (*) Any constant
+    String { fg = hsl("#5CFF4A") },                         --   A string constant: "this is a string"
+    Character { fg = String.fg.desaturate(40).ro(15) },     --   A character constant: 'c', '\n'
     -- Number         { }, --   A number constant: 234, 0xff
-     Boolean        {fg = hsl("#FF5476")}, --   A boolean constant: TRUE, false
+    Boolean { fg = hsl("#FF5476") },                        --   A boolean constant: TRUE, false
     -- Float          { }, --   A floating point constant: 2.3e10
 
-     Identifier     {fg = hsl("#FFC163")}, -- (*) Any variable name
-     Function       {fg = hsl("#3DF9FF")}, --   Function name (also: methods for classes)
+    Identifier { fg = hsl("#FFC163") },    -- (*) Any variable name
+    Function { fg = hsl("#3DF9FF") },      --   Function name (also: methods for classes)
 
-     Statement      {fg = hsl("#FF8438")}, -- (*) Any statement
+    Statement { fg = hsl("#FF8438") },     -- (*) Any statement
     -- Conditional    { }, --   if, then, else, endif, switch, etc.
     -- Repeat         { }, --   for, do, while, etc.
     -- Label          { }, --   case, default, etc.
@@ -100,13 +100,13 @@ local theme = lush(function(injected_functions)
     -- Keyword        { }, --   any other keyword
     -- Exception      { }, --   try, catch, throw
 
-     PreProc        {fg = Function.fg.desaturate(30).ro(20)}, -- (*) Generic Preprocessor
+    PreProc { fg = Function.fg.desaturate(30).ro(20) },       -- (*) Generic Preprocessor
     -- Include        { }, --   Preprocessor #include
     -- Define         { }, --   Preprocessor #define
     -- Macro          { }, --   Same as Define
     -- PreCondit      { }, --   Preprocessor #if, #else, #endif, etc.
 
-     Type           {fg = hsl("#FFa442"), gui = "bold"}, -- (*) int, long, char, etc.
+    Type { fg = hsl("#FFa442"), gui = "bold" },          -- (*) int, long, char, etc.
     -- StorageClass   { }, --   static, register, volatile, etc.
     -- Structure      { }, --   struct, union, enum, etc.
     -- Typedef        { }, --   A typedef
@@ -120,8 +120,8 @@ local theme = lush(function(injected_functions)
 
     -- Underlined     { gui = "underline" }, -- Text that stands out, HTML links
     -- Ignore         { }, -- Left blank, hidden |hl-Ignore| (NOTE: May be invisible here in template)
-     Error          {fg = "white", bg = hsl(360, 100, 68)}, -- Any erroneous construct
-     --Todo           { }, -- Anything that needs extra attention; mostly the keywords TODO FIXME and XXX
+    Error { fg = "white", bg = hsl(360, 100, 68) },         -- Any erroneous construct
+    --Todo           { }, -- Anything that needs extra attention; mostly the keywords TODO FIXME and XXX
 
     -- LspReferenceText            { } , -- Used for highlighting "text" references
     -- LspReferenceRead            { } , -- Used for highlighting "read" references
@@ -130,7 +130,7 @@ local theme = lush(function(injected_functions)
     -- LspCodeLensSeparator        { } , -- Used to color the seperator between two or more code lens.
     -- LspSignatureActiveParameter { } , -- Used to highlight the active parameter in the signature help. See |vim.lsp.handlers.signature_help()|.
 
-     --DiagnosticError            { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+    --DiagnosticError            { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
     -- DiagnosticWarn             { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
     -- DiagnosticInfo             { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
     -- DiagnosticHint             { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
@@ -140,11 +140,11 @@ local theme = lush(function(injected_functions)
     -- DiagnosticVirtualTextInfo  { } , -- Used for "Info" diagnostic virtual text.
     -- DiagnosticVirtualTextHint  { } , -- Used for "Hint" diagnostic virtual text.
     -- DiagnosticVirtualTextOk    { } , -- Used for "Ok" diagnostic virtual text.
-     DiagnosticUnderlineError   {gui = "undercurl", sp="red"} , -- Used to underline "Error" diagnostics.
-     DiagnosticUnderlineWarn    {gui = "undercurl", sp="yellow"} , -- Used to underline "Warn" diagnostics.
-     DiagnosticUnderlineInfo    {gui = "underline", sp="cyan"} , -- Used to underline "Info" diagnostics.
-     DiagnosticUnderlineHint    {gui = "undercurl", sp="cyan"} , -- Used to underline "Hint" diagnostics.
-     DiagnosticUnderlineOk      {gui = "underline", sp="green"} , -- Used to underline "Ok" diagnostics.
+    DiagnosticUnderlineError { gui = "undercurl", sp = "red" },    -- Used to underline "Error" diagnostics.
+    DiagnosticUnderlineWarn { gui = "undercurl", sp = "yellow" },  -- Used to underline "Warn" diagnostics.
+    DiagnosticUnderlineInfo { gui = "underline", sp = "cyan" },    -- Used to underline "Info" diagnostics.
+    DiagnosticUnderlineHint { gui = "undercurl", sp = "cyan" },    -- Used to underline "Hint" diagnostics.
+    DiagnosticUnderlineOk { gui = "underline", sp = "green" },     -- Used to underline "Ok" diagnostics.
     -- DiagnosticFloatingError    { } , -- Used to color "Error" diagnostic messages in diagnostics float. See |vim.diagnostic.open_float()|
     -- DiagnosticFloatingWarn     { } , -- Used to color "Warn" diagnostic messages in diagnostics float.
     -- DiagnosticFloatingInfo     { } , -- Used to color "Info" diagnostic messages in diagnostics float.
@@ -175,7 +175,7 @@ local theme = lush(function(injected_functions)
 
     -- sym"@text.literal"      { }, -- Comment
     -- sym"@text.reference"    { }, -- Identifier
-     --sym"@text.title"        { }, -- Title
+    --sym"@text.title"        { }, -- Title
     -- sym"@text.uri"          { }, -- Underlined
     -- sym"@text.underline"    { }, -- Underlined
     -- sym"@text.todo"         { }, -- Todo
@@ -183,7 +183,7 @@ local theme = lush(function(injected_functions)
     -- sym"@punctuation"       { }, -- Delimiter
     -- sym"@constant"          { }, -- Constant
     -- sym"@constant.builtin"  { }, -- Special
-     sym"@constant.macro"    {fg=Function.fg.ro(-10), gui = "italic"}, -- Define
+    sym "@constant.macro" { fg = Function.fg.ro(-10), gui = "italic" }, -- Define
     -- sym"@define"            { }, -- Define
     -- sym"@macro"             { }, -- Macro
     -- sym"@string"            { }, -- String
@@ -218,7 +218,7 @@ local theme = lush(function(injected_functions)
     -- sym"@preproc"           { }, -- PreProc
     -- sym"@debug"             { }, -- Debug
     -- sym"@tag"               { }, -- Tag
-}
+  }
 end)
 
 -- Return our parsed theme for extension or use elsewhere.
